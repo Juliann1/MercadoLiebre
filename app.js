3f2c676e74ba8b03129b9e206e1d27793fccb272
Juliann1 <juliann_o@hotmail.com>
2022-03-24 20:14:49 -0300

    Small updates for deploy

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/index.html"));
+    res.sendFile(path.resolve(__dirname, "views/index.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/register.html"));
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/login.html"));
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
 });
 
-app.listen(3030, () => console.log("Servidor online"));
+app.listen(PORT, () => console.log("Servidor online"));
